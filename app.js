338b08ff8d2f7444feb4945e45828435636cbbf6
Mike1938 <mmaldonadosan1938@gmail.com>
2021-06-12 23:55:48 -0500

    Created timer function for the game timer adjusted word checking and created few variables to track incorrect word, correct word and words typed

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const textBlock = document.querySelector("#textBlock");
 const textInput = document.querySelector("#textInput");
+const secInputs = document.getElementsByName("secInput");
+const timeBlock = document.getElementById("time");
+const clock = document.querySelector("#clock");
 const displayedWords = document.getElementsByClassName("wordVerfiy");
 const randomNum = (num) => {
     return Math.floor(Math.random() * num);
 }
 let countLocal = 0;
+let wordsTyped = 0;
+let correctWords = 0;
+let incorrectWords = 0;
+let startGame = false;
 const nextWord = () => {
     const allWords = displayedWords.length
     if (allWords === countLocal) {
@@ -14,20 +21,31 @@ const nextWord = () => {
     }
     displayedWords[countLocal].classList.add("next");
     textInput.value = "";
-
+}
+const counter = (seconds) => {
+    let counting = seconds;
+    setInterval(() => {
+        clock.innerText = counting;
+        if (counting === 0) {
+            clearInterval();
+            textInput.disable = true;
+        }
+        counting--
+    }, 1000)
 }
 const checkWord = (word, count = countLocal, spaceHit = false) => {
     const spanWords = displayedWords[count].innerText.trim();
     const letter = word.length - 1;
     if (spaceHit) {
-        console.log(displayedWords[count].className)
         if (word === spanWords) {
-            console.log("is correct");
             if (displayedWords[count].classList.contains("incorrect")) {
                 displayedWords[count].classList.toggle("incorrect");
             }
             displayedWords[count].classList.add("correct");
+            correctWords++
         }
+        wordsTyped++
+        incorrectWords++
     } else if (spanWords[letter] !== word[letter]) {
         displayedWords[count].classList.add("incorrect");
     } else {
@@ -36,9 +54,13 @@ const checkWord = (word, count = countLocal, spaceHit = false) => {
 
     return 1;
 }
-textInput.addEventListener("keyup", function (e) {
-    const wordWriten = textInput.value.trim();
-    console.log(countLocal);
+
+textInput.addEventListener("keydown", function (e) {
+    if (!startGame) {
+        startGame = true
+        counter(60);
+    }
+    let wordWriten = textInput.value.trim();
     if (e.keyCode === 32) {
         countLocal += checkWord(wordWriten, countLocal, true);
         nextWord();
